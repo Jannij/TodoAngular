1e9de6b919eda90c02354985deee6493cc4e5742
Test <Test>
2025-05-27 14:57:43 +0200

    'Completed Task List and styles'

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -17,10 +17,30 @@ export class TodoComponent {
   taskTitle: string = '';
   taskDescription: string = '';
 
-  tasks: Task[] = [];
+  tasks: Task[] = JSON.parse(localStorage.getItem("tasks") || '[]');
+  completedTasks: Task[] = JSON.parse(localStorage.getItem("completedTasks") || '[]');
 
   trackByDate(index: number, task: Task): number {
     return task.date;
+  }
+
+  private remove(index: number, taskList: Task[]): void {
+    taskList.splice(index, 1);
+  }
+
+  removeTask(index: number): void {
+    this.remove(index, this.tasks);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  removeCompletedTask(index: number): void {
+    this.remove(index, this.completedTasks);
+  }
+
+  completeTask(index: number): void {
+    this.completedTasks.push(this.tasks[index]);
+    localStorage.setItem("completedTasks", JSON.stringify(this.completedTasks));
+    this.removeTask(index);
   }
 
   submitTask(): void {
@@ -30,6 +50,8 @@ export class TodoComponent {
         description: this.taskDescription,
         date: Date.now()
       });
+
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
 
       // Clear inputs
       this.taskTitle = '';
