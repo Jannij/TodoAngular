6cb907dc228269bf3f62c4f1bbb5abcc7487c2b8
Test <Test>
2025-06-04 16:16:49 +0200

    'Test Script'

--- src/app/service/task.service.spec.ts
+++ src/app/service/task.service.spec.ts
@@ -1,16 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // import this
+import { TaskService } from './task.service'; // or your actual service
 
-import { TaskService } from './task.service';
-
-describe('TaskService', () => {
+describe('TaskService', (): void => {
   let service: TaskService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach((): void => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // <-- add this here
+    });
     service = TestBed.inject(TaskService);
   });
 
-  it('should be created', () => {
+  it('should be created', (): void => {
     expect(service).toBeTruthy();
   });
 });
